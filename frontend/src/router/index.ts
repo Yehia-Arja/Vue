4588bf7d62becc95fa5113eb4201616308395001
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-30 00:45:58 +0300

    finished the login

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
+import PortalPage from '../pages/PortalPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/login',
     name: 'login',
     component: LoginPage
+  },
+  {
+    path: '/portal',
+    name: 'portal',
+    component: PortalPage
   },
   {
     path: '/',
