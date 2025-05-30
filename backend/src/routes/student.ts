cb2a8e8ab79eb17b074523c5ecb88f871fa1e01f
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-30 08:48:51 +0300

    added a protected route to block unauthorized accesses

--- backend/src/routes/student.ts
+++ backend/src/routes/student.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { getStudents } from '../controllers/studentController'
+import { authenticate } from '../middleware/auth'
 
 const router = Router()
 
-router.get('/', getStudents)
+
+router.get('/', authenticate, getStudents)
 
 export default router
